fc96ac4d558ae1f0bf664e77d3c848311e2a614b
Kevin <kevin.kds80@gmail.com>
2024-11-05 20:51:08 -0300

    add response builder test

---
+++ tests/app/helpers/response-builder.spec.js
@@ -0,0 +1,19 @@
+const { describe, test, expect } = require('@jest/globals')
+const responseBuilder = require('../../../src/app/helpers/response-builder')
+
+describe('Response builder suite', () => {
+
+    test('validate error response object', () => { 
+        const errorResponse = responseBuilder.errorResponse(422, 'error message')
+        expect(errorResponse.error).toBe(422)
+        expect(errorResponse.success).toBe(undefined)
+        expect(errorResponse.data).toBe('error message')
+    })
+
+    test('validate success response object', () => { 
+        const successResponse = responseBuilder.successResponse(201, 'data')
+        expect(successResponse.success).toBe(201)
+        expect(successResponse.error).toBe(undefined)
+        expect(successResponse.data).toBe('data')
+    })
+})
