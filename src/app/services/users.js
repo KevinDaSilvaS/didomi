86e9e8c237e1297989a328739313f3a15784c547
Kevin <kevin.kds80@gmail.com>
2024-11-05 20:36:23 -0300

    add user service logic

---
+++ src/app/services/users.js
@@ -0,0 +1,46 @@
+const userValidator = require('../validators/user')
+const { errorResponse, successResponse } = require('../helpers/response-builder')
+const { 
+    UnprocessableEntity, 
+    InternalServerError,
+    NotFound
+} = require('../errors/errors')
+const { 
+    UserAlreadyRegisteredMessage, 
+    InternalServerErrorMessage, 
+    UserNotFoundMessage
+} = require('../errors/error-messages')
+
+const saveUser = async (user, userRepository) => {
+    try {
+        const validationResult = userValidator.validate(user)
+        if (validationResult.error)
+            return errorResponse(UnprocessableEntity, validationResult.error.message)
+
+        const userExists = await userRepository.getUserByEmail(user.email)
+
+        if (userExists)
+            return errorResponse(UnprocessableEntity, UserAlreadyRegisteredMessage)
+
+        const savedUser = await userRepository.save(user)
+        return successResponse(201, savedUser)
+    } catch (error) {
+        return errorResponse(InternalServerError, InternalServerErrorMessage)
+    }
+}
+
+const getUser = async (userEmail, userRepository) => {
+    try {
+        const user = await userRepository.getUserByEmail(userEmail)
+
+        if (!user)
+            return errorResponse(NotFound, UserNotFoundMessage)
+    } catch (error) {
+        return errorResponse(InternalServerError, InternalServerErrorMessage)
+    }
+}
+
+module.exports = {
+    saveUser,
+    getUser
+}
