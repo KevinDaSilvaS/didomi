402c45a629075a47231d9ad130595ce8e8f8ae3a
Kevin <kevin.kds80@gmail.com>
2024-11-07 20:29:29 -0300

    add event repository

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require('./src/server/server')
 const userRepository = require('./src/db/user-repository')
+const eventsRepository = require('./src/db/events-repository')
 
-app(3000, { userRepository })
+app(3000, { userRepository, eventsRepository })
---
+++ src/db/events-repository.js
@@ -0,0 +1,29 @@
+const process = require('dotenv/config')
+const { eq } = require('drizzle-orm')
+const { drizzle } = require('drizzle-orm/libsql')
+const { createClient } = require('@libsql/client')
+const eventsTable = require('./tables/events')
+
+const client = createClient({ url: process.env?.DB_FILE_NAME || 'file:local.db' })
+const db = drizzle('file:local.db')
+
+const save = async (event, email) => {
+    const data = await db.insert(eventsTable).values({ ...event, email })
+    return data
+}
+
+const getEvents = async (email) => {
+    const consents = await db.select()
+        .from(eventsTable)
+        .where(eq(eventsTable.email, email))
+
+    return consents.map(consent => ({
+        id: consent.id,
+        enabled: consent.enabled
+    }))
+}
+
+module.exports = {
+    save,
+    getEvents
+}
