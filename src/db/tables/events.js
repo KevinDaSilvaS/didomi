638f3b484f906d24b1b21ffdc2c43e3a4272a177
Kevin <kevin.kds80@gmail.com>
2024-11-07 21:32:59 -0300

    add eventId

--- src/db/tables/events.js
+++ src/db/tables/events.js
@@ -1,7 +1,8 @@
-const { integer, sqliteTable, text } = require('drizzle-orm/sqlite-core')
+const { int, integer, sqliteTable, text } = require('drizzle-orm/sqlite-core')
 const usersTable = require('./users')
 
 const eventsTable = sqliteTable("events", {
+  eventId: int().primaryKey({ autoIncrement: true }), 
   id: text().notNull(),
   enabled: integer({ mode: 'boolean' }).notNull(),
   email: text().references(() => usersTable.email, {onDelete: 'cascade'}).notNull()
